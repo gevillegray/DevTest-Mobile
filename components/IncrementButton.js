e6ced832e7a7ffa613a4c42b81f16423dd5c74a1
Guillermo Villegas <gev14b@my.fsu.edu>
2020-03-27 17:31:20 -0700

    increment button component functional

--- components/IncrementButton.js
+++ components/IncrementButton.js
@@ -1,10 +1,16 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function IncrementButton(){
+  const [counter, addOne] = useState(0);
+  var title = "Main Screen Counter - " + String(counter)
+  
   return(
     <View>
-      <Text> test </Text>
+      <Button
+        onPress = {() => addOne(counter + 1)}
+        title = {title}
+      />
     </View>
   )
 }
