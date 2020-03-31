038065ff8925b3a4e73a407a3f4bcd18bf9d1d48
Guillermo Villegas <gev14b@my.fsu.edu>
2020-03-31 14:17:52 -0700

    fixed formatting and added column buttons

--- screens/LunchSpecials.js
+++ screens/LunchSpecials.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import { ActivityIndicator, FlatList, Text, View, TextInput, StyleSheet} from 'react-native';
+import { ActivityIndicator, FlatList, Text, View, TextInput, StyleSheet, Button} from 'react-native';
 
 export default LunchSpecials = () => {
   const [isLoading, setLoading] = useState(true);
   const [data, setData] = useState([]);
+  var headTable = ['id', 'name', 'dish'];
 
   useEffect(() => {
     fetch('https://devapi.inertia.systems/devtest/lunchspecials.php')
@@ -15,33 +16,44 @@ export default LunchSpecials = () => {
 
   return (
     <View style={{ flex: 1, padding: 24 }}>
-    <TextInput
-          style={styles.textInputStyle}
-          // onChangeText={text => this.SearchFilterFunction(text)}
-          // value={this.state.text}
-          underlineColorAndroid="transparent"
-          placeholder="Search Here"
-        />
-
+      <TextInput
+            style={styles.textInputStyle}
+            // onChangeText={text => this.SearchFilterFunction(text)}
+            // value={this.state.text}
+            underlineColorAndroid="transparent"
+            placeholder="Search Here"
+      />
+      <View style={styles.button}>
+          <Button title = "ID"/>
+          <Button title = "Restaurant"/>
+          <Button title = "Dish"/>
+      </View>
       {isLoading ? <ActivityIndicator/> : (
-        <FlatList
-          data={data}
-          keyExtractor={({ id }, index) => id}
-          renderItem={({ item }) => (
-            <Text>{item.name}, {item.dish}</Text>
-          )}
-        />
+        <View>
+          <FlatList
+            data={data}
+            keyExtractor={({ id }, index) => id}
+            renderItem={({ item }) => (
+              <View style={styles.container}>
+                <Text>{item.id}</Text>
+                <Text style ={styles.paragraph}>{item.name}</Text>
+                <Text>{item.dish}</Text>
+              </View>
+            )}
+          />
+        </View>
       )}
-    </View>
+      </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-between',
+    backgroundColor: '#ecf0f1',
+    flexDirection:'row',
+    alignItems:'baseline',
   },
   textInputStyle: {
     height: 40,
@@ -50,4 +62,17 @@ const styles = StyleSheet.create({
     borderColor: '#009688',
     backgroundColor: '#FFFFFF',
   },
+  paragraph: {
+   margin: 24,
+   fontSize: 16,
+   marginVertical: 4,
+   textAlign: 'center',
+ },
+ button: {
+   flex: 1,
+   justifyContent: 'space-between',
+   backgroundColor: '#ecf0f1',
+   flexDirection:'row',
+   alignItems:'baseline',
+ },
 });
